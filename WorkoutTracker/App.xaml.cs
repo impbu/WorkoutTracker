4b6306466a6613994ce22eba2b10e8ab786118d9
impbu <impliedbunnies@gmail.com>
2023-08-15 22:41:34 -0500

    initial commit

---
+++ WorkoutTracker/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Workout_Tracker
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ WorkoutTracker/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Workout_Tracker
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
